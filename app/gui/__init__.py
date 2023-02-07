da0be585c3df01399fdc6198fabb74ed16642faf
ArielMAJ <ariel.maj@hotmail.com>
2023-02-07 13:31:43 -0300

    [ADD] [Task 003] Added demonstration GUI MVP.
    Co-authored-by: tauanesales <taysales6@gmail.com>

---
+++ app/gui/__init__.py
@@ -0,0 +1,5 @@
+"""
+This file will initialize this subfolder for easier importing.
+"""
+
+from .floating_window import FloatingWindow
---
+++ app/main.py
@@ -0,0 +1,17 @@
+"""
+This file will start the program.
+"""
+
+import gui
+
+
+def main():
+    """
+    This scrip will start the program.
+    """
+    root = gui.FloatingWindow()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
